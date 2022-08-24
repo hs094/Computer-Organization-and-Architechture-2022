ce054ea652044b9741b7de483f1594cfebc98b5d
iamhardikat11 <iamhardikat11@gmail.com>
2022-08-24 15:53:57 +0530

    Full Adder

--- Q1/Full_Adder.v
+++ Q1/Full_Adder.v
@@ -9,3 +9,17 @@
     # Hardik Soni 20CS30023
 # ---------------------------------------------------------
 
+module FullAdder(
+    input a,
+    input b,
+    input c_in,
+    output S,
+    output c_out
+);
+
+wire c1, c2, sum
+halfAdder ha1(a, b, sum, c1);
+halfAdder ha2(sum, c_in, S, c2);
+or gate1(c_out, c1, c2);
+
+endmodule
