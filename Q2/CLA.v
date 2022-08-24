dd8f5bc36bc89460d2ba56e699bb3d6f6c97473b
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2022-08-24 15:28:42 +0530

    Create CLA.v

---
+++ Q2/CLA.v
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------
+    # COMPUTER ORGANIZATION LABORATORY
+    # AUTUMN SEMESTER 2022
+    # Assignment 3
+    # Problem 2
+    # Group No. 60
+    # Abhay Kumar Keshari 20CS10001
+    # Hardik Soni 20CS30023
+# ---------------------------------------------------------
