a4ba95e024d769b66709896eca7aa25051bcf166
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2022-08-24 15:29:31 +0530

    Update RCA.v

--- Q1/RCA.v
+++ Q1/RCA.v
@@ -1,8 +1,8 @@
 # ---------------------------------------------------------
     # COMPUTER ORGANIZATION LABORATORY
     # AUTUMN SEMESTER 2022
-    # Assignment 2
-    # Problem 3
+    # Assignment 3
+    # Problem 1
     # Group No. 60
     # Abhay Kumar Keshari 20CS10001
     # Hardik Soni 20CS30023
