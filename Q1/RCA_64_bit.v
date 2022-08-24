c5c9db4f264fb51aa3d0164dcf155108bd193fb7
iamhardikat11 <iamhardikat11@gmail.com>
2022-08-24 17:08:25 +0530

    Added File RCA_64_bit file

--- Q1/RCA_64_bit.v
+++ Q1/RCA_64_bit.v
@@ -9,4 +9,15 @@
     # Hardik Soni 20CS30023
 # ---------------------------------------------------------
 
-module
+module RCA_64_bit(
+    input [63:0] a,
+    input [63:0] b,
+    input c_in,
+    output [63:0] s,
+    output c_out
+);
+
+wire carry;
+RCA_32_bit ra1(a[31:0], b[31:0], c_in, s[31:0], carry);
+RCA_32_bit ra2(a[63:32], b[63:32], carry, s[63:32], c_out);
+endmodule
