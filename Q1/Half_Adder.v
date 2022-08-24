a6817235b2923ff8b3b9f76feb31933a175cdbcc
iamhardikat11 <iamhardikat11@gmail.com>
2022-08-24 15:38:51 +0530

    Adding Module Half-Adder

--- Q1/Half_Adder.v
+++ Q1/Half_Adder.v
@@ -8,3 +8,19 @@
     # Hardik Soni 20CS30023
 # ---------------------------------------------------------
 
+module halfAdder(
+    input a,
+    input b,
+    output S,
+    output C
+);
+    /*
+    *     Inputs:             Outputs:
+	*   a = 0, b = 0        S = 0, C = 0
+	*   a = 0, b = 1        S = 1, C = 0
+	*   a = 1, b = 0        S = 1, C = 0
+	*   a = 1, b = 1        S = 0, C = 1
+    */
+    assign S = a ^ b;
+    assign C = a & b; 
+end module
