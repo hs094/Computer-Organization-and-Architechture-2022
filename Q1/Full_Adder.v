1881fc480726c74f535285cde83ae57732a185c1
iamhardikat11 <iamhardikat11@gmail.com>
2022-08-24 16:09:21 +0530

    Made Changes to Full_Adder.v and Created RCA_4bit.v

--- Q1/Full_Adder.v
+++ Q1/Full_Adder.v
@@ -12,14 +12,14 @@
 module FullAdder(
     input a,
     input b,
-    input c_in,
+    input c_0,
     output S,
-    output c_out
+    output c
 );
 
 wire c1, c2, sum
 halfAdder ha1(a, b, sum, c1);
-halfAdder ha2(sum, c_in, S, c2);
-or gate1(c_out, c1, c2);
+halfAdder ha2(sum, c_0, S, c2);
+or gate1(c, c1, c2);
 
 endmodule
