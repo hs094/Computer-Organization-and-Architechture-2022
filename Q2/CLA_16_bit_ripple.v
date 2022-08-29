539146140ce6fbef738a385688720bc1f2fa3018
hardik <iamhardikat11@gmail.com>
2022-08-30 00:04:17 +0530

    16 bit CLA

--- Q2/CLA_16_bit_ripple.v
+++ Q2/CLA_16_bit_ripple.v
@@ -9,3 +9,17 @@
 *    # Hardik Soni 20CS30023
 *---------------------------------------------------------
 */ 
+module CLA_16_bit_ripple(
+    input [15:0] a,
+    input [15:0] b,
+    input c_in,
+    output [15:0] s,
+    output c_out
+);
+
+wire [2:0] c;
+CLA_4_bit cl1(a[3:0], b[3:0], c_in, s[3:0], c[2]);
+CLA_4_bit cl2(a[7:4], b[7:4], c[2], s[7:4], c[1]);
+CLA_4_bit cl3(a[11:8], b[11:8], c[1], s[11:8], c[0]);
+CLA_4_bit cl4(a[15:12], b[15:12], c[0], s[15:12], c_out);
+endmodule
--- Q2/CLA_16_bit_ripple_TestBench.v
+++ Q2/CLA_16_bit_ripple_TestBench.v
@@ -9,3 +9,29 @@
 *    # Hardik Soni 20CS30023
 *---------------------------------------------------------
 */ 
+module CLA_16_bit_ripple_TestBench;
+    reg [15:0] a,b;
+    reg c_in;
+    wire [15:0] s;
+    wire c_out;
+
+    CLA_16_bit_ripple uut(
+        .a(a),
+        .b(b),
+        .c_in(c_in),
+        .s(s),
+        .c_out(c_out)
+    );
+    
+    initial begin
+    $moniter("a = %d, b = %d, c_in = %d, s = %d, c_out = %d", a,b,c_in,s,c_out);
+    // Initialise TestCases
+    a = 16'd34783; b = 16'd3443; c_in = 1;
+    #100;
+    a = 16'd3783; b = 16'd673443; c_in = 0;
+    #100;
+    a = 16'd58783; b = 16'd33; c_in = 1;
+    #100;
+    a = 16'd457; b = 16'd45887; c_in = 0;
+    end
+endmodule
