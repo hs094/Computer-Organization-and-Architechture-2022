e7ca2bb5ee069aa923e61a7e4f94203863f5a498
hardik <iamhardikat11@gmail.com>
2022-08-30 00:25:32 +0530

    CLA -16 bit with LCU

--- Q2/CLA_16_bit_LCU.v
+++ Q2/CLA_16_bit_LCU.v
@@ -10,5 +10,21 @@
 *---------------------------------------------------------
 */ 
 module CLA_16_bit_LCU(
-    input [15:0]
+    input [15:0] a,
+    input [15:0] b,
+    input c_in,
+    output [15:0] s,
+    output c_out,
+    output p,
+    output g
 );
+wire [3:0] P,G;
+wire [4:1] C;
+
+CLA_4_bit_augmented cl1(a[3:0], b[3:0], c_in, P[0], G[0], s[3:0]);
+CLA_4_bit_augmented cl2(a[7:4], b[7:4], C[1], P[1], G[1], s[3:0]);
+CLA_4_bit_augmented cl3(a[11:8], b[11:8], C[2], P[2], G[2], s[3:0]);
+CLA_4_bit_augmented cl4(a[15:12], b[15:12], C[3], P[3], G[3], s[3:0]);
+LCA_Unit lu(P,G,C,p,g);
+assign c_out = C[4];
+endmodule
--- Q2/CLA_16_bit_LCU_TestBench.v
+++ Q2/CLA_16_bit_LCU_TestBench.v
@@ -10,3 +10,32 @@
 *---------------------------------------------------------
 */ 
 module CLA_16_bit_LCU_TestBench;
+    reg[15:0] a,b;
+    reg c_in;
+    wire[15:0]s;    
+    wire c_out;
+    wire p,g;
+
+    CLA_16_bit_LCU uut(
+        .a(a),
+        .b(b),
+        .s(s),
+        .c_out(c_out),
+        .p(p),
+        .g(g);
+    );
+
+initial begin
+    $moniter("a = %d, b = %d, s = %d, c_out = %d, p = %d, g = %d", a, b, s, c_out, p, g);
+    // Initialize Inputs
+	a = 16'd32445; b = 16'd16785; c_in = 0;
+	#100;
+	a = 16'd32145; b = 16'd16785; c_in = 0;
+	#100;
+	a = 16'd12500; b = 16'd40535; c_in = 1;
+	#100;
+	a = 16'd25021; b = 16'd40535; c_in = 1;
+	#100;
+	a = 16'd25020; b = 16'd01535; c_in = 0;
+    end
+endmodule
