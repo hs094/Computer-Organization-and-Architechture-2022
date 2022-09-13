aa9b827461a80adc6ab398df6dc2bb8612e75ff4
abhay-k47 <99312837+abhay-k47@users.noreply.github.com>
2022-09-13 23:27:49 +0530

    Update add_by_one.v

--- Assignment 5/add_by_one.v
+++ Assignment 5/add_by_one.v
@@ -1,15 +1,12 @@
-module add_by_one(a);
+module add_by_one(
+    input [0:3] in,
+    output [0:3] out
+    );
 
-    input [3:0]a;
-
-
-    wire [3:0]P,C;
-    assign P = a;
-    assign C[0] = 1;
-    assign C[1] = P[0];
-    assign C[2] = P[0] & P[1];
-    assign C[3] = P[0] & P[1] & P[2];
-    assign p = a[3] & a[2] & a[1] & a[0];
-    assign a = P ^ C;
+    assign out[0] = ~a[0];
+    assign out[1] = a[1] ^ (a[0]);
+    assign out[2] = a[2] ^ (a[1] & a[0]);
+    assign out[3] = a[3] ^ (a[2] & a[1] & a[0]);
 
 endmodule
+
